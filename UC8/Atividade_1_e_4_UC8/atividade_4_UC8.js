b9e24a90feabae74d79b94293a31a4efd82a8f56
Bruno <brunomatcomp@outlook.com>
2022-09-23 23:17:44 -0300

    Resposta da atividade 4 e 1 - UC8

---
+++ UC8/Atividade_1_e_4_UC8/atividade_4_UC8.js
@@ -0,0 +1,20 @@
+//Atividade 4 UC8
+let participantesCadastrados = ["Fulano", "Ciclano", "Beltrano"]
+let dataEvento
+let dataAtual
+let candidatoAoCadastro 
+let idadeCandidato
+        // Cenário: Inscrição em um evento para um determinado dia após o dia atual.
+console.log ("Entre com a data do evento:")  // candidato ao evento digita a data do evento   
+if(dataEvento>dataAtual){    
+         if(participantesCadastrados.length<100){
+               console.log ("Informe sua idade:") 
+                // aqui ---> comando para leitura
+             if(idade>=18){   
+               console.log ("Informe seu nome:");
+               // aqui ---> comando para leitura
+                participantesCadastrado.push(candidatoAoCadasto)
+                console.log ("Cadastro realizado com sucesso!");
+            }else{console.log ("Menor de idade não pode ser cadastrado!")}
+     }else{console.log ("Não há mais vagas!")}
+    }else{console.log ("Data inválida!")}
